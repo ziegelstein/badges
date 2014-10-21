42b362c0777b70b0a09b8e3c0612cc790540a17c
TheBuli22 <julius2207@hotmail.de>
2014-10-21 18:22:36 +0200

    Argumente in badgeCommand implementiert

--- src/de/psfanserver/badge/badgeCommand.java
+++ src/de/psfanserver/badge/badgeCommand.java
@@ -12,30 +12,44 @@ public class badgeCommand implements CommandExecutor {
 			return false;
 		}
 		
-		//add (/badge add <badgeID> <spielername/uuid>
-		if(args[0].equalsIgnoreCase("add")){
-			
-			if(args.length == 3){
-				//badge hinzufügen
-			}
-			else{
-				cs.sendMessage("/badge add <badgeID> <PlayerName/UUID>");
-			}
+		try{
+			int id = Integer.parseInt(args[0]);
+		} catch(NumberFormatException nfe){
+			cs.sendMessage("Keine gültige ID, es sind nur Ziffern erlaubt.");
+			return true;
 		}
 		
-		
-		//remove (/badge remove <badgeID> <spilername/uuid>
-		if(args[0].equalsIgnoreCase("remove")){
+		if(args.length == 1){
 			
-			if(args.length == 3){
-				//badge entfernen
-			}
-			else{
-				cs.sendMessage("/badge remove <badgeID> <PlayerName/UUID>");
-			}
+			//Zeige informationen Über ein Badge (Name, Item, Lore, Enchantment)
+			
+			return true;
 		}
 		
-		
+		if(args[1].equalsIgnoreCase("create")){
+			//erstellt Badge mit der ID "id"
+		} else if(args[1].equalsIgnoreCase("name")){
+			//zeigt oder setzt den Namen für das Badge
+			
+		} else if(args[1].equalsIgnoreCase("item")){
+			//zeigt oder setzt das Item für das Badge
+			
+		} else if(args[1].equalsIgnoreCase("lore")){
+			//zeigt oder setzt die Lores für das Badge
+			
+		} else if(args[1].equalsIgnoreCase("enchantments")){
+			//zeigt oder setzt die enchantments für das Badge
+			
+		} else if(args[1].equalsIgnoreCase("users")){
+			//zeigt alle Spieler die Badge "id" haben
+			
+		} else if(args[1].equalsIgnoreCase("user")){
+			//Zeigt an ob ein Spieler das Badge hat; bzw. setzt oder entfernt ein Badge von einem Spieler
+			// badge <id> user add/remove <name/uuid/"all">
+		} else{
+			cs.sendMessage("Argument "+args[1]+" nicht gefunden.");
+			return true;
+		}
 		return false;
 	}
 
